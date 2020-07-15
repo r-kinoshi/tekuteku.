8eb88c53a49e58481ed1475ebc68a40c8a68923a
rica441 <pokari.san12@gmail.com>
2020-07-15 16:58:21 +0900

    feat: 投稿機能追加 #7

---
+++ components/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+   <div class="posts overflow-scroll mb-24">
+    <post />
+   </div>
+ </div>
+</template>
+
+<script>
+import Post from '~/components/Post.vue'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
+    <header class="flex justify-center border-b border-gray-300">
+     <div class="logo">
+       <img src="/images/logo.png" class="w-20 py-2">
+     </div>
+     <img src="/images/letter.svg" class="absolute w-6 post">
+   </header>
+   <posts />
   </div>
 </template>
 
 <script>
+import Logo from '~/components/Logo.vue'
+import Posts from '~/components/Posts.vue'
+
 export default {
+  components: {
+   Posts
+ }
 }
 </script>
 
-<style>
+<style scoped>
+.post {
+ top: 12px;
+ right: 10px;
+}
 </style>
