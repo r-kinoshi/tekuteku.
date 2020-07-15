e6e8dab9e7c512ac02c0791e0b0b3054a3923441
rica441 <pokari.san12@gmail.com>
2020-07-15 15:55:30 +0900

    refactor: footerをcomponents #5

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bottom-navigation bg-white flex fixed bottom-0 h-12 w-full border-t border-gray-300">
+    <div class="nav-item w-1/3 flex justify-center">
+      <a><img src="/images/home.svg" class="h-6 my-3"></a>
+    </div>
+    <div class="nav-item w-1/3 flex justify-center">
+      <a><img src="/images/follow.svg" class="h-6 my-3"></a>
+    </div>
+    <div class="nav-item w-1/3 flex justify-center">
+      <a><img src="/images/profile.svg" class="h-6 my-3"></a>
+    </div>
+  </div>
+</template>
