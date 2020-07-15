b771f970a8fc5e1ca4b11688cc0bfe2fe82d648a
rica441 <pokari.san12@gmail.com>
2020-07-15 17:28:32 +0900

    feat: ユーザーのマイページUI作成 #9

---
+++ components/User.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="user p-4 flex justify-between">
+   <div class="user-info flex">
+     <div class="avatar mr-4">
+       <a><img src="/images/post1.jpg" class="w-12 h-12 rounded-full"></a>
+     </div>
+     <div class="displayName vertical-middle">
+       <p>rika123</p>
+     </div>
+   </div>
+   <div class="follow-btn vertical-middle">
+     <button>Follow</button>
+   </div>
+ </div>
+</template>
---
+++ pages/users/_id.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="mb-32">
+   <div class="user flex justify-between px-8 my-8">
+     <div class="flex">
+       <div class="user-avatar mr-4">
+         <img src="/images/post1.jpg" class="w-12 h-12 rounded-full">
+       </div>
+       <div class="user-name vertical-middle">
+         <p>rika123</p>
+       </div>
+     </div>
+     <button class="text-sm">ログアウト</button>
+   </div>
+   <div class="tab flex justify-around">
+     <div class="post-count text-center">
+       Post
+       </br>
+       <span class="text-xs">0</span>
+     </div>
+     <div class="text-center">
+       Following
+       </br>
+       <span class="text-xs">0</span>
+     </div>
+     <div class="text-center">
+       Follower
+       </br>
+       <span class="text-xs">0</span>
+     </div>
+   </div>
+ </div>
+</template>
