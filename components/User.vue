82dfb8aaf8ac7a1a03efd741712dfea6e35fe791
rica441 <pokari.san12@gmail.com>
2020-07-19 16:46:36 +0900

    docs: フォロー機能追加 #50

--- components/User.vue
+++ components/User.vue
@@ -10,17 +10,24 @@
        <nuxt-link :to="`/users/${user.uid}`">
         <p>{{ user.displayName }}</p>
        </nuxt-link>
-
      </div>
    </div>
    <div class="follow-btn vertical-middle" v-if="!isCurrentUser">
-     <el-button>Follow</el-button>
+     <el-button v-if="!followed" @click="follow">Follow</el-button>
+     <el-button v-else>Unfollow</el-button>
    </div>
  </div>
 </template>
 
 <script>
+import { db } from '~/plugins/firebase'
+
   export default {
+    data () {
+      return {
+        followed: false
+      }
+    },
     props: ['user'],
     computed: {
       currentUser () {
@@ -29,6 +36,13 @@
       isCurrentUser () {
         return this.currentUser.uid === this.user.id
       }
+    },
+    methods: {
+      async follow () {
+        await db.collection('users').doc(this.currentUser.uid).collection('followings').doc(this.user.id).set({user: this.user.id})
+        await db.collection('users').doc(this.user.id).collection('followers').doc(this.currentUser.uid).set({ user: this.currentUser.uid})
+        this.followed = true
+     }
     }
   }
 </script>
