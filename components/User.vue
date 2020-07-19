85f0ec312dbc2c30bd4ea0bee479489a927b720d
rica441 <pokari.san12@gmail.com>
2020-07-19 16:57:21 +0900

    docs:  アンフォロー機能追加 #50

--- components/User.vue
+++ components/User.vue
@@ -14,7 +14,7 @@
    </div>
    <div class="follow-btn vertical-middle" v-if="!isCurrentUser">
      <el-button v-if="!followed" @click="follow">Follow</el-button>
-     <el-button v-else>Unfollow</el-button>
+     <el-button v-else @click="unfollow">Unfollow</el-button>
    </div>
  </div>
 </template>
@@ -42,7 +42,12 @@ import { db } from '~/plugins/firebase'
         await db.collection('users').doc(this.currentUser.uid).collection('followings').doc(this.user.id).set({user: this.user.id})
         await db.collection('users').doc(this.user.id).collection('followers').doc(this.currentUser.uid).set({ user: this.currentUser.uid})
         this.followed = true
-     }
+     },
+      async unfollow () {
+        await db.collection('users').doc(this.currentUser.uid).collection('followings').doc(this.user.id).delete()
+        await db.collection('users').doc(this.user.id).collection('followers').doc(this.currentUser.uid).delete()
+        this.followed = false
+      }
     }
   }
 </script>
