4d5e2b7aeb945602ba6d5bdceba54eab68e7ae7d
rica441 <pokari.san12@gmail.com>
2020-07-26 15:07:19 +0900

    style: footerにアイコン追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="bottom-navigation bg-white flex fixed bottom-0 h-12 w-full border-t border-gray-300">
-    <div class="nav-item w-1/3 flex justify-center">
+    <div class="nav-item w-1/5 flex justify-center">
       <nuxt-link to="/"><img src="/images/home.svg" class="h-6 my-3"></nuxt-link>
     </div>
-    <div v-if="isAuthenticated" class="nav-item w-1/3 flex justify-center">
+    <div v-if="isAuthenticated" class="nav-item w-1/5 flex justify-center">
+      <nuxt-link to="/users"><img src="/images/search (3).svg" class="h-6 my-3"></nuxt-link>
+    </div>
+    <div v-if="isAuthenticated" class="nav-item w-1/5 flex justify-center">
       <nuxt-link to="/users"><img src="/images/follow.svg" class="h-6 my-3"></nuxt-link>
     </div>
-    <div v-if="isAuthenticated" class="nav-item w-1/3 flex justify-center">
+     <div v-if="isAuthenticated" class="nav-item w-1/5 flex justify-center">
+      <nuxt-link to="/users"><img src="/images/heart (1).svg" class="h-6 my-3"></nuxt-link>
+    </div>
+    <div v-if="isAuthenticated" class="nav-item w-1/5 flex justify-center">
       <nuxt-link  :to="`/users/${currentUser.uid}`"><img src="/images/profile.svg" class="h-6 my-3"></nuxt-link>
     </div>
   </div>
