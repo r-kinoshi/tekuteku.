a569c09afe60cdbcf235c1a03db2159e0ec6e3e8
rica441 <pokari.san12@gmail.com>
2020-07-31 19:21:42 +0900

    fix: tailwindcssの設定変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 　module.exports = {
+  purge: [
+    "./pages/**/*.vue", 
+    "./components/**/*.vue", 
+    "./plugins/**/*.vue",
+    "./static/**/*.vue",
+    "./store/**/*.vue"
+  ],
   theme: {},
   variants: {},
   plugins: []
